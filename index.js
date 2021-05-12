b3e48392d2c150ff8fff3df037a3c41a211bc0f7
suraj soan kamble <35141388+surajsoankamble@users.noreply.github.com>
2021-05-12 18:04:21 +0530

    covid cases plotting on map
    using javascript,css,html
    using mapbox
    data from about-corona.net api

---
+++ index.js
@@ -0,0 +1,44 @@
+function updateMap() {
+    fetch('https://corona-api.com/countries')
+        .then(response => response.json())
+        .then(resData => {
+            console.log(resData.data);
+            resData.data.forEach(element => {
+                latitude = element.coordinates.latitude;
+                longitude = element.coordinates.longitude;
+
+                totalCases=element.latest_data.confirmed;
+                if(totalCases<1000000){
+                    color = '#ffcccb';
+                }
+                else if(totalCases>=1000000 && totalCases<=10000000){
+                    color='#ff4040';
+                }
+                else{
+                    color='#ff0000';
+                }
+
+                // mark on map
+                var marker = new mapboxgl.Marker({
+                        draggable: false,
+                        color: color
+                    })
+
+                var minPopup = new mapboxgl.Popup()
+
+                minPopup.setText(`Total Cases: ${totalCases}`)
+                marker.setPopup(minPopup)
+
+                marker.setLngLat([longitude, latitude])
+                marker.addTo(map);
+
+                
+                
+
+                
+            });
+        })
+
+}
+
+updateMap();
